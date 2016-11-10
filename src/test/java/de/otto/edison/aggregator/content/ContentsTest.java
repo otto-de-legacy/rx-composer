4fb04d6f1c41bf830e79ddd3f94599e445636a46
Guido Steinacker <guido.steinacker@otto.de>
2016-11-10 20:09:40 +0100

    More Tests

---
+++ src/test/java/de/otto/edison/aggregator/content/ContentsTest.java
@@ -0,0 +1,101 @@
+package de.otto.edison.aggregator.content;
+
+
+import de.otto.edison.aggregator.content.Content.Availability;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static de.otto.edison.aggregator.content.AbcPosition.A;
+import static de.otto.edison.aggregator.content.Content.Availability.AVAILABLE;
+import static de.otto.edison.aggregator.content.Content.Availability.EMPTY;
+import static de.otto.edison.aggregator.content.Content.Availability.ERROR;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+public class ContentsTest {
+
+    @Test
+    public void shouldAddAvailableContent() {
+        // given
+        final Contents contents = new Contents();
+        // when
+        contents.add(someContent("some content", AVAILABLE));
+        // then
+        assertThat(contents.hasErrors(), is(false));
+        assertThat(contents.getContents(), hasSize(1));
+        assertThat(contents.getContent(A).isPresent(), is(true));
+        assertThat(contents.getEmpty(), is(empty()));
+        assertThat(contents.getErrors(), is(empty()));
+    }
+
+    @Test
+    public void shouldAddEmptyContent() {
+        // given
+        final Contents contents = new Contents();
+        // when
+        contents.add(someContent("", EMPTY));
+        // then
+        assertThat(contents.hasErrors(), is(false));
+        assertThat(contents.getContents(), is(empty()));
+        assertThat(contents.getContent(A).isPresent(), is(false));
+        assertThat(contents.getEmpty(), contains(A));
+        assertThat(contents.getErrors(), is(empty()));
+    }
+
+    @Test
+    public void shouldAddErrorContent() {
+        // given
+        final Contents contents = new Contents();
+        // when
+        contents.add(someContent("", ERROR));
+        // then
+        assertThat(contents.hasErrors(), is(true));
+        assertThat(contents.getContents(), is(empty()));
+        assertThat(contents.getContent(A).isPresent(), is(false));
+        assertThat(contents.getEmpty(), is(empty()));
+        assertThat(contents.getErrors(), contains(A));
+    }
+
+    private Content someContent(final String body, final Availability availability) {
+        return new Content() {
+            @Override
+            public Position getPosition() {
+                return A;
+            }
+
+            @Override
+            public int getIndex() {
+                return 0;
+            }
+
+            @Override
+            public boolean hasContent() {
+                return !body.isEmpty();
+            }
+
+            @Override
+            public String getBody() {
+                return body;
+            }
+
+            @Override
+            public Headers getHeaders() {
+                return new Headers(null);
+            }
+
+            @Override
+            public LocalDateTime getCreated() {
+                return LocalDateTime.now();
+            }
+
+            @Override
+            public Availability getAvailability() {
+                return availability;
+            }
+        };
+    }
+}
