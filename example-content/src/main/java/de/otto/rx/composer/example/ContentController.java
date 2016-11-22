647df37232248e3cf5d74d7fede6df5625cf2a8a
Guido Steinacker <guido.steinacker@otto.de>
2016-11-22 18:02:46 +0100

    Added parameter to example controller

--- example-content/src/main/java/de/otto/rx/composer/example/ContentController.java
+++ example-content/src/main/java/de/otto/rx/composer/example/ContentController.java
@@ -2,19 +2,28 @@ package de.otto.rx.composer.example;
 
 import de.otto.rx.composer.Plan;
 import de.otto.rx.composer.content.Contents;
+import de.otto.rx.composer.content.Parameters;
 import de.otto.rx.composer.http.HttpClient;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import static com.damnhandy.uri.template.UriTemplate.fromTemplate;
+import static com.google.common.collect.ImmutableMap.of;
 import static de.otto.rx.composer.Plan.planIsTo;
 import static de.otto.rx.composer.content.AbcPosition.A;
 import static de.otto.rx.composer.content.AbcPosition.B;
 import static de.otto.rx.composer.content.Parameters.emptyParameters;
+import static de.otto.rx.composer.content.Parameters.parameters;
 import static de.otto.rx.composer.providers.ContentProviders.fetchViaHttpGet;
 import static de.otto.rx.composer.steps.Steps.forPos;
 import static javax.ws.rs.core.MediaType.TEXT_HTML_TYPE;
 
+/**
+ * Fetches two contents from two different "microservices". An optional request-parameter 'name' is forwarded to the
+ * 'hello' service, which is returning 'Hello &lt;name&gt;'.
+ */
 @Controller
 public class ContentController {
 
@@ -22,9 +31,12 @@ public class ContentController {
 
     @RequestMapping(path = "/", produces = "text/html")
     @ResponseBody
-    public String getContent() {
+    public String getContent(final @RequestParam(required = false) String name) {
         final Plan plan = defaultPlan();
-        final Contents contents = plan.execute(emptyParameters());
+        final Parameters parameters = name != null
+                ? parameters(of("name", name))
+                : emptyParameters();
+        final Contents contents = plan.execute(parameters);
         return "<html>"
                 + "<body>"
                 + "<div id=A>" + contents.getContent(A).get().getBody() + "</div>"
@@ -34,7 +46,7 @@ public class ContentController {
 
     private Plan defaultPlan() {
         return planIsTo(
-                forPos(A, fetchViaHttpGet(client, "http://localhost:8080/hello", TEXT_HTML_TYPE)),
+                forPos(A, fetchViaHttpGet(client, fromTemplate("http://localhost:8080/hello{?name}"), TEXT_HTML_TYPE)),
                 forPos(B, fetchViaHttpGet(client, "http://localhost:8080/somethingElse", TEXT_HTML_TYPE))
         );
     }
--- example-content/src/main/java/de/otto/rx/composer/example/HelloController.java
+++ example-content/src/main/java/de/otto/rx/composer/example/HelloController.java
@@ -2,6 +2,7 @@ package de.otto.rx.composer.example;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -9,7 +10,7 @@ public class HelloController {
 
     @RequestMapping(path = "/hello", produces = "text/html")
     @ResponseBody
-    public String sayHello() {
-        return "<h1>Hello World!</h1>";
+    public String sayHello(final @RequestParam(defaultValue = "World") String name) {
+        return "<h1>Hello " + name + "!</h1>";
     }
 }
