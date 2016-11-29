c8a271c7a2abba42e12ee52a26096a258a3f03a5
Guido Steinacker <guido.steinacker@otto.de>
2016-11-29 19:45:55 +0100

    Added missing tests for CompositeContent

--- src/test/java/de/otto/rx/composer/providers/SelectingContentProviderTest.java
+++ src/test/java/de/otto/rx/composer/providers/SelectingContentProviderTest.java
@@ -8,10 +8,13 @@ import rx.Observable;
 import java.time.LocalDateTime;
 import java.util.Iterator;
 
+import static com.google.common.collect.ImmutableList.*;
 import static de.otto.rx.composer.content.AbcPosition.X;
 import static de.otto.rx.composer.content.Headers.emptyHeaders;
 import static de.otto.rx.composer.content.Parameters.emptyParameters;
+import static de.otto.rx.composer.providers.ContentProviders.fetchAll;
 import static de.otto.rx.composer.providers.ContentProviders.fetchFirst;
+import static de.otto.rx.composer.providers.ContentProviders.fetchFirstMatching;
 import static java.time.LocalDateTime.now;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -26,7 +29,7 @@ public class SelectingContentProviderTest {
     @Test
     public void shouldFetchFirstWithContent() {
         // given
-        final ContentProvider contentProvider = fetchFirst(ImmutableList.of(
+        final ContentProvider contentProvider = fetchFirst(of(
                 (position, parameters) -> just(new TestContent(X, "Foo")),
                 (position, parameters) -> just(new TestContent(X, "Bar"))
         ));
@@ -38,9 +41,41 @@ public class SelectingContentProviderTest {
     }
 
     @Test
+    public void shouldFetchAllWithContent() {
+        // given
+        final ContentProvider contentProvider = fetchAll(of(
+                (position, parameters) -> just(new TestContent(X, "Foo")),
+                (position, parameters) -> just(new TestContent(X, "Bar"))
+        ));
+        // when
+        final Observable<Content> result =  contentProvider.getContent(X, emptyParameters());
+        // then
+        final Content content = result.toBlocking().single();
+        assertThat(content.isComposite(), is(true));
+        assertThat(content.getBody(), is("Foo\nBar"));
+    }
+
+    @Test
+    public void shouldFetchFirstMatchingPredicate() {
+        // given
+        final ContentProvider contentProvider = fetchFirstMatching(
+                (content) -> content.getBody().contains("a"),
+                of(
+                        (position, parameters) -> just(new TestContent(X, "Foo")),
+                        (position, parameters) -> just(new TestContent(X, "Bar"))
+                )
+        );
+        // when
+        final Observable<Content> result =  contentProvider.getContent(X, emptyParameters());
+        // then
+        final Content content = result.toBlocking().single();
+        assertThat(content.getBody(), is("Bar"));
+    }
+
+    @Test
     public void shouldSelectFirstNotEmpty() {
         // given
-        final ContentProvider contentProvider = fetchFirst(ImmutableList.of(
+        final ContentProvider contentProvider = fetchFirst(of(
                 (position, parameters) -> just(new TestContent(X, "")),
                 (position, parameters) -> just(new TestContent(X, "Hello World"))
         ));
@@ -55,7 +90,7 @@ public class SelectingContentProviderTest {
     @Test
     public void shouldHandleEmptyContents() {
         // given
-        final ContentProvider contentProvider = fetchFirst(ImmutableList.of(
+        final ContentProvider contentProvider = fetchFirst(of(
                 (position, parameters) -> just(new TestContent(X, "")),
                 (position, parameters) -> just(new TestContent(X, ""))
         ));
@@ -69,7 +104,7 @@ public class SelectingContentProviderTest {
     @Test
     public void shouldHandleExceptions() {
         // given
-        final ContentProvider contentProvider = fetchFirst(ImmutableList.of(
+        final ContentProvider contentProvider = fetchFirst(of(
                 (position, parameters) -> {
                     throw new IllegalStateException("Bumm!!!");
                 },
@@ -88,7 +123,7 @@ public class SelectingContentProviderTest {
         final ContentProvider nestedProvider = mock(ContentProvider.class);
         when(nestedProvider.getContent(X, emptyParameters())).thenReturn(just(new TestContent(X, "Foo")));
         // and
-        final ContentProvider fetchFirstProvider = fetchFirst(ImmutableList.of(
+        final ContentProvider fetchFirstProvider = fetchFirst(of(
                 nestedProvider,
                 (position, parameters) -> just(new TestContent(X, "Bar"))
         ));
