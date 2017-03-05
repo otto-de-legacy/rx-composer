419b5f386eb85154f127d87da8f78242952026ad
Guido Steinacker <guido.steinacker@otto.de>
2017-03-05 12:53:17 +0100

    Fixed path of @ComponentScan

--- example-content/src/main/java/de/otto/rx/composer/example/Server.java
+++ example-content/src/main/java/de/otto/rx/composer/example/Server.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("de.otto.rx.composer.example")
+@ComponentScan("de.otto.rx.composer")
 @SpringBootApplication
 public class Server {
     public static void main(String[] args) {
